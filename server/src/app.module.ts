65f2a5beb98a86a5e27aaa59f22939a8278f99f0
Yiqun Zheng <16813562+yiqunzheng@users.noreply.github.com>
2025-09-27 20:03:08 -0700

    clean up files

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,11 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { RobotModule } from './robot/robot.module';
 
 @Module({
   imports: [RobotModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
